4abbeb4ef065def8626199abdf4a843bcb70f1a7
Harpz <76766221+harpzng@users.noreply.github.com>
2024-10-16 18:21:57 -0300

    base activity

---
+++ app/src/main/java/ca/mobiledev/remind/BaseActivity.kt
@@ -0,0 +1,58 @@
+package ca.mobiledev.remind
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navView: NavigationView = findViewById(R.id.navView)
+
+        val toggle= ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener { menuItem->
+            when(menuItem.itemId){
+                R.id.nav_home->{
+                    //bring the home page to the front
+                }
+
+                R.id.nav_settings->{
+                    //start settings activity
+                }
+
+                R.id.nav_history->{
+                    //start score activity
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
--- app/src/main/java/ca/mobiledev/remind/MainActivity.kt
+++ app/src/main/java/ca/mobiledev/remind/MainActivity.kt
@@ -2,52 +2,13 @@ package ca.mobiledev.remind
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.internal.NavigationMenu
-import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var drawerLayout: DrawerLayout
-
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        toolbar.setTitle(R.string.app_name)
-        setSupportActionBar(toolbar)
-
-
-        drawerLayout = findViewById(R.id.drawer_layout)
-
-        val navView: NavigationView = findViewById(R.id.navView)
-
-        val toggle= ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        navView.setNavigationItemSelectedListener { menuItem->
-            when(menuItem.itemId){
-                R.id.nav_home->{
-                    //bring the home page to the front
-                }
-
-                R.id.nav_settings->{
-                    //start settings activity
-                }
-
-                R.id.nav_history->{
-                    //start score activity
-                }
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
+          layoutInflater.inflate(R.layout.activity_main, findViewById(R.id.content_frame))
         }
     }
-}
