2352bd7ca82de3338f1eae38c59332ff58e87767
Harpz <76766221+harpzng@users.noreply.github.com>
2024-12-04 11:04:23 -0400

    dark mode

--- app/src/main/java/ca/mobiledev/remind/MainActivity.kt
+++ app/src/main/java/ca/mobiledev/remind/MainActivity.kt
@@ -3,9 +3,11 @@ package ca.mobiledev.remind
 import android.content.ActivityNotFoundException
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatDelegate
 import ca.mobiledev.remind.abstractclasses.BaseActivity
 import ca.mobiledev.remind.pathwaysgame.Game
 
@@ -14,6 +16,16 @@ class MainActivity : BaseActivity() {
 
         super.onCreate(savedInstanceState)
           layoutInflater.inflate(R.layout.activity_main, findViewById(R.id.content_frame))
+
+        //set dark/light mode based on stored setting in shared pref
+        val sharedPreferences = getSharedPreferences("AppSettings", MODE_PRIVATE)
+        val savedMode = sharedPreferences.getString("Theme", "system")
+        val currentMode = when (savedMode) {
+            "dark" -> AppCompatDelegate.MODE_NIGHT_YES
+            "light" -> AppCompatDelegate.MODE_NIGHT_NO
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(currentMode)
 
         val pathwaysButton = findViewById<ImageButton>(R.id.btnPathways)
         pathwaysButton.setBackgroundColor(resources.getColor(R.color.blue))
